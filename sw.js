ee2050f7981ef105373124ac845f02c857d78caa
eugeniahwd <eugeniahwda@gmail.com>
2025-08-04 10:54:14 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "photobooth-cache-v1";
+const CACHE_NAME = "photobooth-cache-v1.2";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -23,4 +23,5 @@ self.addEventListener("fetch", (event) => {
       return response || fetch(event.request);
     })
   );
+
 });
